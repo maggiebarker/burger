e9c813f3a79314c4637e9295b574ad7a4e5642ce
maggiebarker <maggieb.06@gmail.com>
2018-01-06 18:28:09 -0700

    seeds/schema, db set up

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tuscan Chicken Burger Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Garlic Pepper Burger');
+
+INSERT INTO menu (item) VALUES ('Western Bacon Burger');
+INSERT INTO menu (item) VALUES ('Tuscan Chicken Burger');
+INSERT INTO menu (item) VALUES ('Double Cheese Burger');
+INSERT INTO menu (item) VALUES ('Garlic Pepper Burger');
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Single Cheeseburger');
+INSERT INTO menu (item) VALUES ('Teriyaki Steak Burger');
+INSERT INTO menu (item) VALUES ('Pulled Pork Sandwich');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Tater Tots', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Cole Slaw', 0);
+INSERT INTO menu (item, entree) VALUES ('Soup of the Day', 0);
+
